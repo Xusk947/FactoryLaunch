ce44d73d10ef6e1d891f40e61a70912505ef6747
shift_develope <53366878+shiftdevy@users.noreply.github.com>
2020-07-18 17:34:16 +0300

    Added new lib. Some minor fixes.

--- Assets/Functions/Utils/playerControl.lua
+++ Assets/Functions/Utils/playerControl.lua
@@ -1,25 +1,28 @@
-function playerKeyControl()
+function playerKeyControl(time)
+  local Time = time
+  local times = 50
+  local speed = 5
     if(love.keyboard.isDown("w")) then
-      for i = 0, 5 do
-        y = y - 50 * dt
+      for i = 0, times do
+        player.y = player.y - speed * Time
       end
     end
 
     if(love.keyboard.isDown("s")) then
-      for i = 0, 5 do
-        y = y + 50 * dt
+      for i = 0, times do
+        player.y = player.y + speed * Time
       end
     end
 
     if(love.keyboard.isDown("a")) then
-      for i = 0, 5 do
-        x = x - 50 * dt
+      for i = 0, times do
+        player.x = player.x - speed * Time
       end
     end
 
     if(love.keyboard.isDown("d")) then
-      for i = 0, 5 do
-        x = x + 50 * dt
+      for i = 0, times do
+        player.x = player.x + speed * Time
       end
     end
 end
