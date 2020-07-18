2a25ab18fff08200e49f9f8e2b7ec4e81e24dd34
shift_develope <53366878+shiftdevy@users.noreply.github.com>
2020-07-18 15:42:15 +0300

    Added new lib. Some minor fixes.

---
+++ Assets/Functions/Utils/player.lua
@@ -0,0 +1,3 @@
+player = {}
+player.x = 0
+player.y = 0
--- main.lua
+++ main.lua
@@ -1,21 +1,21 @@
 require('Assets/Functions/mapFunc')
 require('Assets/Functions/Presets/presets')
 require('Assets/Functions/Utils/playerControl')
+require('Assets/Functions/Utils/Cameras/playerCamera')
+require('Assets/Functions/Utils/player')
 
 function love.load()
-  x = 0
-  y = 0
 
   windowSet(750, 100)
   loadMap('Assets/Maps/randomMap.lua')
-  player = love.graphics.newImage('Assets/Images/Character.png')
+  playerSprite = love.graphics.newImage('Assets/Images/Character.png')
 end
 
 function love.update(dt)
-  playerKeyControl()
+  playerKeyControl(dt)
 end
 
 function love.draw()
   drawMap()
-  love.graphics.draw(player, x, y)
+  love.graphics.draw(playerSprite, player.x, player.y)
 end
