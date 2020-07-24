5df87eb30932bfa59bd8db1b3b2bd9aff63454dd
shift_develope <53366878+shiftdevy@users.noreply.github.com>
2020-07-24 20:13:41 +0300

    soft Lib
    this lib will be added tomorrow to the github.

---
+++ main.lua
@@ -0,0 +1,20 @@
+--import section
+require('project/content/variables')
+require('project/content/modules')
+
+--main "chunk"
+function love.load()
+  
+end
+
+
+
+function love.update(dt)
+
+end
+
+
+
+function love.draw()
+
+end
---
+++ project/core/content/variables.lua
@@ -0,0 +1,9 @@
+--soft lib variables
+
+--camera
+camera = {}
+camera.x = 0
+camera.y = 0
+camera.scaleX = 1
+camera.scaleY = 1
+camera.rotation = 0
